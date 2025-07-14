347033e719716cd548b18ea758f80a66e5eb2efe
codewithvijay07 <vikasrajpoot735@gmail.com>
2025-07-14 22:28:01 +0530

    Deignwithvikas

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Wireframing & Prototyping", "User Research", "Interaction Design (IxD)","Responsive Design"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
